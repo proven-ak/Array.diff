56018856679120416357da0637c4a77fd76664ef
proven_ak <proven@ya.ru>
2024-11-10 16:29:11 +0300

    10.11.24  не работает

--- main.py
+++ main.py
@@ -16,16 +16,49 @@ array_diff([1,2,2,2,3],[2]) == [1,3]
 
 #def array_diff(a,b)
 
-a = [2,4,2,2,3]
-b = [2]
+a = [[1,2,3], [1,3], [1,2], [3,2,1]]
+b = [1,2]
 
-new_a = a
+c = []
+cc = []
+aa = []
+aaa = []
+bb = []
+bbb = 0
+new_a = []
 
-for i in range(len(a)-1, -1, -1):
-    if a[i] == b[0]:
-        new_a.pop(i)
-c = new_a
+#print(len(a))
+for i in range(0, len(a), 1):
+    aa = a[i]
+    for j in range(0, len(aa), 1):
+        aaa = aa[j]
+        for k in range(0, len(b), 1):
+            print(i,j,k)
+            #bbb = len(b[k])
 
-#return c
+            if len(bb) != 0:
+                if aaa[j] == b[k]:
+                    aaa.pop(j)
+                cc.append(aaa[j])
 print(c)
 
+
+"""
+a = [[1,2,3], [1]]
+b = [1]
+
+c = []
+new_a = []
+#print(len(a))
+for i in range(0, len(a), 1):
+    new_a = a[i]
+    for j in range(len(a[i])-1, 0, -1):
+        if len(b) != 0:
+            if new_a[j] == b[0]:
+                new_a.pop(j)
+                #print(i,j,new_a)
+    c.append(new_a)
+print(c)
+"""
+
+#return c
